30bbc48adaa753aa7dfaba6c0d1f3770b7750d3d
Vezro <45973533+Vezro@users.noreply.github.com>
2019-01-11 13:56:45 -0500

    ww

--- src/pages/testPage1.vue
+++ src/pages/testPage1.vue
@@ -10,6 +10,7 @@
             {{ index + 1 }}
           </q-chip>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
           <div class="row justify-around" style="background-color:#F6F6F9;">
              <div class="col-6">
 
@@ -78,6 +79,7 @@
 
              </div>
           </div>
+
         </p>
 
         <div class="row justify-center" style="margin-bottom: 50px;">
