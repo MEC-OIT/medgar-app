d2d493c063324cdeee42f912d82c1aa8fcfd8286
Vezro <45973533+Vezro@users.noreply.github.com>
2019-01-11 13:34:51 -0500

    r

--- src/pages/pageTest1.vue
+++ src/pages/pageTest1.vue
@@ -1,3 +1,15 @@
-<div class="">
-  hecl
-</div>
+<template lang="html">
+  <q-page>
+    <div class="">
+      Heck
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
