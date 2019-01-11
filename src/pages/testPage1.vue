0ea2f64c0c168c32b0271aa6e1513d4f23bb6d32
Vezro <45973533+Vezro@users.noreply.github.com>
2019-01-11 14:00:20 -0500

    w

--- src/pages/testPage1.vue
+++ src/pages/testPage1.vue
@@ -10,7 +10,15 @@
             {{ index + 1 }}
           </q-chip>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-
+          <q-card inline class="q-ma-sm">
+            <q-card-media>
+              <img class=""src="statics/images/img1c.png">
+            </q-card-media>
+            <q-card-title>
+              Courgars...
+            <q-rating size='12px' v-model="stars" :max="5"  />
+          </q-card-title>
+          </q-card>
         </p>
 
         <div class="row justify-center" style="margin-bottom: 50px;">
