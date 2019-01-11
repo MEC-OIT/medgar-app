17546070ded8fd7561f9421cea6dd5914a91a0cd
Vezro <45973533+Vezro@users.noreply.github.com>
2019-01-11 13:54:47 -0500

    www

--- src/pages/testPage1.vue
+++ src/pages/testPage1.vue
@@ -10,15 +10,74 @@
             {{ index + 1 }}
           </q-chip>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          <q-card inline class="q-ma-sm">
-            <q-card-media>
-              <img class=""src="statics/images/img2c.png">
-            </q-card-media>
-            <q-card-title>
-              Courgas...
-            <q-rating size='12px' v-model="stars" :max="5"  />
-          </q-card-title>
-        </q-card>
+          <div class="row justify-around" style="background-color:#F6F6F9;">
+             <div class="col-6">
+
+               <q-card inline class="q-ma-sm">
+                 <q-card-media>
+                   <img class=""src="statics/images/img1c.png">
+                 </q-card-media>
+                 <q-card-title>
+                   Courgars...
+                 <q-rating size='12px' v-model="stars" :max="5"  />
+               </q-card-title>
+             </q-card>
+
+             <q-card inline class="q-ma-sm">
+               <q-card-media>
+                 <img class="image-size-1"src="statics/images/img3w.png">
+               </q-card-media>
+               <q-card-title>
+                 Writers on...
+               <q-rating size='12px' v-model="stars" :max="5"  />
+             </q-card-title>
+           </q-card>
+
+           <q-card inline class="q-ma-sm">
+             <q-card-media>
+               <img class="image-size-1"src="statics/images/img4j.png">
+             </q-card-media>
+             <q-card-title>
+               Jazzy jazz..
+             <q-rating size='12px' v-model="stars" :max="5"  />
+           </q-card-title>
+         </q-card>
+             </div>
+
+             <div class="col-6">
+
+               <q-card inline class="q-ma-sm">
+                 <q-card-media>
+                   <img class=""src="statics/images/img2c.png">
+                 </q-card-media>
+                 <q-card-title>
+                   Courgas...
+                 <q-rating size='12px' v-model="stars" :max="5"  />
+               </q-card-title>
+             </q-card>
+
+             <q-card inline class="q-ma-sm">
+               <q-card-media>
+                 <img class=""src="statics/images/img5d.png">
+               </q-card-media>
+               <q-card-title>
+                 Dr. Rudy Crew...
+               <q-rating size='12px' v-model="stars" :max="5"  />
+             </q-card-title>
+           </q-card>
+
+           <q-card inline class="q-ma-sm">
+             <q-card-media>
+               <img class=""src="statics/images/img1c.png">
+             </q-card-media>
+             <q-card-title>
+               Courgas...
+             <q-rating size='12px' v-model="stars" :max="5"  />
+           </q-card-title>
+         </q-card>
+
+             </div>
+          </div>
         </p>
 
         <div class="row justify-center" style="margin-bottom: 50px;">
